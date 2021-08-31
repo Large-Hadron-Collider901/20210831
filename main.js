8405a4815ae2e8fbbd2a55c836caeaf5238419fc
Skye M <skyemc608@gmail.com>
2021-08-31 11:15:04 -0500

    more class notes

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // console.log("hello-world!");
 
+// select the element we are listening for
 let theWholeBody = document.querySelector("body");
 
 theWholeBody.addEventListener("keydown", (evt) => {
+  if (evt.key === "o") {
+    theWholeBody.style.backgroundColor = "orange";
+  }
+
   console.log(`Yo you pressed the ${evt.key}`);
 });
